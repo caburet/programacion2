0eec9ec49013bb77b9964f232ddee9fb69887993
Matias-Bussetti <desarrollos.informaticos.bsstt@gmail.com>
2023-08-28 21:12:30 -0300

    Mi Primer Cambio

--- participantes.py
+++ participantes.py
@@ -1,2 +1,6 @@
 print ("Acá estuvo el profesor Joel Partida. Saludos a todos")
 print ("Esto es un primer cambio hecho por Joel Partido y revisado por Joel Partida")
+
+
+
+print("😂 Realizado por Matias Bussetti")
