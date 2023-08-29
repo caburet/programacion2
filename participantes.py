072baacd3cacc0d9b2dc38124bf0479a10db5487
Juan Chaparro <chapaPR@gmail.com>
2023-08-28 21:11:31 -0300

    feature/chaparrojuan

--- participantes.py
+++ participantes.py
@@ -1,2 +1,9 @@
 print ("Acá estuvo el profesor Joel Partida. Saludos a todos")
 print ("Esto es un primer cambio hecho por Joel Partido y revisado por Joel Partida")
+
+
+
+
+
+
+print ("Acá estuvo Juan Chaparro.")
