72e8b9b33268cac8a2b210f9ce8b87b37c37a1b8
jaoquin <joaquinhernandezj.4@gmail.com>
2023-08-28 21:25:35 -0300

    agrego el nombre al archivo

--- participantes.py
+++ participantes.py
@@ -1,2 +1,3 @@
-print ("Acá estuvo el profesor Joel Partida. Saludos a todos")
-print ("Esto es un primer cambio hecho por Joel Partido y revisado por Joel Partida")
+print("Acá estuvo el profesor Joel Partida. Saludos a todos")
+print("Esto es un primer cambio hecho por Joel Partido y revisado por Joel Partida")
+print("hecho en clase, joaquin hernandez")
