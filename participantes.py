c1959fb7151846c2038011e22d8f0cdaedac9974
Emiliano Correa <emiliano.correa.amigo@gmail.com>
2023-08-28 22:11:41 -0300

    fibonacci?

--- participantes.py
+++ participantes.py
@@ -1,3 +1,8 @@
 print ("Acá estuvo el profesor Joel Partida. Saludos a todos")
 print ("Esto es un primer cambio hecho por Joel Partido y revisado por Joel Partida")
 print("cambio por Emiliano Correa")
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
