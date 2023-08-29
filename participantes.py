5f6bdc199c764fce9b3ae3cea4106d175347527a
NicoValenciano <lordmacako@gmail.com>
2023-08-28 21:25:04 -0300

    Saluda Nico Valenciano

--- participantes.py
+++ participantes.py
@@ -1,2 +1,3 @@
 print ("Acá estuvo el profesor Joel Partida. Saludos a todos")
 print ("Esto es un primer cambio hecho por Joel Partido y revisado por Joel Partida")
+print("Acá Nico Valenciano. Un saludo para todos los que me conocen")
