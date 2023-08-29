14e42c92f12bb9c16f7892ab2a56a7fea1c9a83d
Eros <erosmarziani@gmail.com>
2023-08-28 21:06:04 -0300

    Mi primer cambio

--- participantes.py
+++ participantes.py
@@ -1,2 +1,4 @@
 print ("Acá estuvo el profesor Joel Partida. Saludos a todos")
 print ("Esto es un primer cambio hecho por Joel Partido y revisado por Joel Partida")
+
+print("Aca modifico el codigo Eros Marziani.")
