584b519e9c9ff2e92eee4bf4dcf76bd9adbbea79
Luciano Balot <lnbalot02@gmail.com>
2023-08-30 09:01:27 -0300

    Mi primer cambio - Balot

--- participantes.py
+++ participantes.py
@@ -1,2 +1,4 @@
 print ("Acá estuvo el profesor Joel Partida. Saludos a todos")
 print ("Esto es un primer cambio hecho por Joel Partido y revisado por Joel Partida")
+
+print("Hola me llamo Luciano Balot y este es mi primer cambio.")
