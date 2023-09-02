88a2c2f2c95ea3fc37f76f343448b1562d058278
NeisaBelleggia <belleggian@gmail.com>
2023-09-02 13:39:30 -0300

    mi primer cambio

--- participantes.py
+++ participantes.py
@@ -1,2 +1,3 @@
 print ("Acá estuvo el profesor Joel Partida. Saludos a todos")
 print ("Esto es un primer cambio hecho por Joel Partido y revisado por Joel Partida")
+print ("Acá estuvo la alumna Neisa Belleggia, de Carhué. Saludos a todos.")
