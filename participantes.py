5b4eaebeb8a486c4757320bcc7880d34b80743b1
CristinaMorales <crisvikim@hotmail.com>
2023-09-04 12:03:35 -0300

    editado por alumna Critina

--- participantes.py
+++ participantes.py
@@ -1,3 +1,3 @@
-print ("Acá estuvo el profesor Joel Partida. Saludos a todos")
+print ("Acá estuvo la alumna Critina Morales. Saludos a todos")
 print ("Esto es un primer cambio hecho por Joel Partido y revisado por Joel Partida")
 print ("Generando conflictos!!! MUEJEJE")
