68f038677f575d959e331968156938bac6904ae6
NeisaBelleggia <belleggian@gmail.com>
2023-09-04 21:35:54 -0300

    Cambio Neisa Belleggia

--- participantes.py
+++ participantes.py
@@ -5,7 +5,7 @@ print("Hola, soy camila piergentili")
 
 print ("Acá estuvo la alumna Critina Morales. Saludos a todos")
 print ("Esto es un primer cambio hecho por Joel Partido y revisado por Joel Partida")
-
+print ("Acá estuvo Neisa Belleggia :)")
 
 
 
