b41567d9ed67cebe01a0b18bbafbbe264e4ba558
pedroweyland <weylandpedro@gmail.com>
2023-09-04 21:40:14 -0300

    borre mi print extra

--- participantes.py
+++ participantes.py
@@ -1,10 +1,8 @@
 print ("Acá estuvo el profesor Joel Partida. Saludos a todos")
 print("Esto es un primer cambio hecho por Joel Partido y revisado por Joel Partida")
+
 print("hecho en clase, joaquin hernandez")
-print ("Esto es un primer cambio hecho por Joel Partido y revisado por Joel Partida")
 print("Mi primer cambioo, Matias Farias")
-
-
 print("Primer cambio Gabriel Ponce")
 print("Acá Nico Valenciano. Un saludo para todos los que me conocen")
 print("Hola, soy camila piergentili")
@@ -14,9 +12,7 @@ print ("Acá estuvo Juan Chaparro.")
 print("Estoy en clase haviendo codigo y soy Mikeas Correa.")
 print("Soy MAria Eugenia Losada")
 print("Hola me llamo Luciano Balot y este es mi primer cambio.")
-
 print("Evelin Paumgertner was here")
-
 print(" es mi primer commit")
 print("Estoy escribiendo, soy Bruno")
 print("Primer cambio Gabriel Ponce")
@@ -26,4 +22,3 @@ print("Soy Pedro Weyland y este es mi primer commit :)")
 print("cambio por Emiliano Correa")
 print ("estoy haciendo esto en clase y mi nombre es Daniel D'Onofrio")
 print ("Facundo Reiseng commit")
-print("Soy Pedro Weyland. :)")
