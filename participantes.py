8a66742403f10da1497d2f2aa14b73d922bd8925
GastonIvanHaagGomez <gasel88@hotmail.com>
2023-09-04 21:21:23 -0300

    cambio

--- participantes.py
+++ participantes.py
@@ -1,11 +1,11 @@
 print ("Acá estuvo el profesor Joel Partida. Saludos a todos")
 print ("Esto es un primer cambio hecho por Joel Partido y revisado por Joel Partida")
-<<<<<<< HEAD
+
 
 
 print("Hola me llamo Luciano Balot y este es mi primer cambio.")
 
 print(" es mi primer commit")
-=======
+
 print("Primer cambio Gabriel Ponce")
->>>>>>> 1f2d387de57fce6d944296f209a1675f4edcd567
+
