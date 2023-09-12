9c9dc18206431af1f9505f472dd3995a22e838c2
pedroweyland <weylandpedro@gmail.com>
2023-09-12 02:47:39 -0300

    ej 4,5,6,7,8

--- Tp3.py
+++ Tp3.py
@@ -1,5 +1,6 @@
 from os import system
-
+from functools import reduce
+import os 
 global separador
 separador = "------------------------"
 
@@ -101,11 +102,158 @@ def Ej_3():
 
 
 #Ejercicio 4
-
+def primera_letra():
+    
+    while True:
+        letra = input("Escriba la LETRA que comienze su palabra deseada: ")
+        
+        if len(letra) == 1:
+            return letra.upper()
+        else:
+            print("Tiene que escribir una letra")
+        
 def Ej_4():
     #4. Dado un diccionario de palabras y sus definiciones, crea una lista que contenga sólo las palabras que 
     #   comienzan con una letra específica (por ejemplo, "a") indicada por el usuario, utilizando list comprehensions.
+
+    definiciones = {
+        "Adulto" : "Persona con mas de 18 anios",
+        "Perro" : "Animal de 4 patas domesticado por el humano",
+        "Ciudad" : "Poblacion donde habita un gran conjunto de personas",
+        "Aplicacion" : "Un programa de software diseñado para realizar una tarea específica o brindar una funcionalidad específica.",   
+        "Algoritmo" : "Un conjunto de instrucciones lógicas y bien definidas que resuelven un problema o realizan una tarea.",    
+    }
+    #Usuario carga que letra quiere que comience
+    letra = primera_letra()
+
+    # guarda a la lista las palabnras que empiezen con la letra elegida por el usuario 
+    palabras_con_a = [palabra for palabra in definiciones if letra == palabra[0]]
+
     print(separador)
+
+    longitud = len(palabras_con_a)
+    #Muestra de resultados
+    if longitud > 1: 
+        print(f"Estas son las palabras que empiezan con la {letra}: {palabras_con_a}")
+
+    elif longitud == 1:
+        print(f"Esta esta es la unica palabra que empieza con la letra {letra}: {palabras_con_a}")
+
+    else:
+        print(f"No existe palabras que empieza con la letra {letra}")
+
+#Ejercicio 5
+def es_primo(num):
+    
+    for i in range(2,num):
+        if num % i == 0:
+            return False
+    
+    return True
+def Ej_5():
+    #5. Dado un rango de números, crea una lista que contenga todos los números primos dentro de ese rango utilizando list comprehensions.
+    texto = "Escriba un numero para saber si existe numeros primos entre el 2 hasta el numero deseado: "
+    
+    maximo = obtener_num_mayor_0(texto)
+    
+    #Creacion de lista con el rango de numeros dado por el usuario
+    lista = list(range(2,maximo + 1))
+
+    #Suma a la 'lista_primos' todo numero primo
+    
+    lista_primos = [numero for numero in lista if es_primo(numero)]
+    
+    #opcion usando 'filter
+    #lista_primos = list(filter(es_primo, lista))
+    
+    #Resultado
+    longitud = len(lista_primos)
+    if longitud > 1:
+        print(f"Estos son los numeros primos que hay entre el cero y el {maximo}: {lista_primos}")
+    elif longitud == 1:
+        print(f"Este es el unico numero primero que hay entre el cero y el {maximo}: {lista_primos}")
+
+#Ejercicio 6
+
+
+def Ej_6():
+    #6. Dado un diccionario de personas y sus edades, crea una lista que contenga solo los nombres de las personas 
+    #   personas cuya edad es mayor a unaedad ingresada por el usuario, utilizando list comprehensions.
+    personas = {
+        "Carlos" : 10,
+        "Pedro" : 65,
+        "Juan" : 40,
+        "Tevez" : 20,
+        "Riquelme" : 25,
+        "Gustavo" : 30,
+    }
+
+    edad = obtener_num_mayor_0("Escriba la edad minima de las personas que quiere visualizar: ")
+
+    lista_mayores = [clave for clave in personas if edad < personas[clave] ]
+
+    longitud = len(lista_mayores)
+    if longitud > 1:
+        print(f"Estas son las personas cuya edad es mayor a {edad}: {lista_mayores}")
+    elif longitud ==1:
+        print(f"Esta es la unica persona con la edad mayo a {edad}: {lista_mayores}")
+    else:
+        print("No existe una persona que tenga mayor edad que esa")
+
+#Ejercicio 7
+
+#Contar vocales forma basica
+def contador_vocales(palabra):
+    contador = 0
+    for letra in palabra:
+        if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u":
+            contador += 1
+    return contador
+
+def Ej_7():
+    #7. Implemente un programa que le pida una palabra al usuario y cuenta la cantidad de vocales en ella. 
+    #   El programa deberá pedirle palabras  al usuario hasta que éste introduzca la palabra “salir”.
+
+    seguir = True
+    
+    while seguir:
+        
+        palabra = input("Escriba una palabra para ver cuantas vocales tiene, si quiere salir escriba 'salir': ").lower()
+        print(separador)
+        if palabra != "salir":
+            
+            #cant_vocales = contador_vocales(palabra)
+            
+            #funcion para saber si la letra es vocal
+            es_vocal = lambda letra: letra in "aeiou"
+            
+            #filter para recorrer letra por letra y ver si es vocal, filter vuelve true o false, si es true se suma a la lista
+            lista_vocales = list(filter(es_vocal, palabra))
+
+            cant_vocales = len(lista_vocales)
+
+            print(f"La cantidad de vocales que tiene la palabra es de: {cant_vocales}")
+            print(separador)
+
+        else:
+            print("Saliendo...")
+            seguir = False    
+        
+#Ejercicio 8 
+
+def Ej_8():
+    #8. Dada una lista con elementos duplicados, crea una nueva lista que contenga solo los elementos únicos utilizando list comprehensions.
+
+    lista = [2 ,1 ,4 ,2 ,2 , 5, 4, 1, 2, 3, 6, 3]
+
+    #lista_unicos = list(set(lista))
+    lista_unicos = []
+    [lista_unicos.append(elem) for elem in lista if elem not in lista_unicos]
+    
+    print(lista_unicos)
+
+
+
 def MainMenu():
     print("********************") 
     print("**  1 - Ejercicio **")
@@ -168,49 +316,50 @@ while (sigue):
         print(separador)
         Ej_4()
         print(separador)
-        system("pause")
-        system("cls")
-        #input("Presione Enter para continuar")
-        #os.system("clear")
+        #system("pause")
+        #system("cls")
+        input("Presione Enter para continuar")
+        os.system("clear")
+
     elif opcion == "5":
         print("5 - Ejercicio")
         print(separador)
-        #Ej_5()
+        Ej_5()
         print(separador)
-        system("pause")
-        system("cls")
-        #input("Presione Enter para continuar")
-        #os.system("clear")
+        #system("pause")
+        #system("cls")
+        input("Presione Enter para continuar")
+        os.system("clear")
 
     elif opcion == "6":
         print("6 - Ejercicio")
         print(separador)
-        #Ej_6()
+        Ej_6()
         print(separador)
-        system("pause")
-        system("cls")
-        #input("Presione Enter para continuar")
-        #os.system("clear")
+        #system("pause")
+        #system("cls")
+        input("Presione Enter para continuar")
+        os.system("clear")
 
     elif opcion == "7":
         print("7 - Ejercicio")
         print(separador)
-        #Ej_7()
+        Ej_7()
         print(separador)
-        system("pause")
-        system("cls")
-        #input("Presione Enter para continuar")
-        #os.system("clear")
+        #system("pause")
+        #system("cls")
+        input("Presione Enter para continuar")
+        os.system("clear")
 
     elif opcion == "8":
         print("8 - Ejercicio")
         print(separador)
-        #Ej_8()
+        Ej_8()
         print(separador)
-        system("pause")
-        system("cls")
-        #input("Presione Enter para continuar")
-        #os.system("clear")
+        #system("pause")
+        #system("cls")
+        input("Presione Enter para continuar")
+        os.system("clear")
 
     elif opcion == "9":
         print("9 - Ejercicio")
