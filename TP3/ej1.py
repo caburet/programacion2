e08857d8bc438f03b981ad63484a90357696b98e
jaoquin <joaquinhernandezj.4@gmail.com>
2023-09-18 09:09:26 -0300

    ejercicio 1

--- TP3/ej1.py
+++ TP3/ej1.py
@@ -1,1 +1,8 @@
 ##1. Implemente una función que dada una lista de números, devuelva una nueva lista que contenga el cuadrado de cada número utilizando list comprehensions.
+
+lista1 = [1,2,3,4,5,6,7]
+nueva_lista = []
+nueva_lista = [elem**2 for elem in lista1]
+
+print(f"La lista original es: {lista1}")
+print(f"La lista elevada al cuadrado es: {nueva_lista}")
